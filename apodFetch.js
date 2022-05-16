75134377cfb142131e9558796f3d1f50e91b1904
JharnaKarki1 <jharna.karki@gmail.com>
2022-05-16 02:10:35 -0500

    Update apodFetch.js

--- apodFetch.js
+++ apodFetch.js
@@ -1,21 +1,23 @@
-$(document).ready(
-    function () {    
-    $("#view_button").click(getPicture);
- });
+$(document).ready(function () {
 
- const getPicture = async () => {
-    const response = await fetch("https://api.nasa.gov/planetary/apod?api_key=0AJH3YGVbm8W6Ay9sNegG4sedHNzr6zBkoOh53WA&date=",{
-     method: "GET",
-     data : $("#date").val() 
-           
- })
-    .then((response) => response.json())
-    .then(showPicture);
-      
-}
-   function showPicture(data) {   
-     
-    $("#pic").attr("src", data.url); 
-    $("h2").text(data.title);
-    };
-    
+   //using pure promise syntax
+   $("#view_button").click(() => {
+
+       let date = $("#date").val();
+       $("#pic").show();
+       //sending request here
+       fetch(`https://api.nasa.gov/planetary/apod?date=${date}&api_key=0AJH3YGVbm8W6Ay9sNegG4sedHNzr6zBkoOh53WA`)
+           //getting the promise here from the fetch request
+           .then(response => response.json())
+           //getting and using the response here
+           .then(data => {
+               $("#pic").attr("src", data.url);
+               $("#titleOfPhoto").text(data.title);
+               
+           })
+           //handling error here if any occur
+           .catch(err => console.log(err.message))
+
+
+   })
+})
